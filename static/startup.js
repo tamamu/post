c5e41d85d1e289b87f2fed28779902832a50db3e
tamamu <tamamu.1r1s@gmail.com>
2018-02-27 12:58:10 +0900

    Hide page source

--- static/startup.js
+++ static/startup.js
@@ -3,5 +3,6 @@ window.onload = () => {
   let renderer = new PageRenderer();
   let content = document.getElementById('main-content');
   content.innerHTML = renderer.render(content.innerHTML);
+  content.style.visibility = 'visible';
 }
 
